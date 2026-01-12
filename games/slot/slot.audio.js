1074e4092dcbeedf0756c4b4d68d9d61bb78158b
travel director <wordycow0001@gmail.com>
2026-01-12 22:03:01 +0900

    Update slot.audio.js

--- games/slot/slot.audio.js
+++ games/slot/slot.audio.js
@@ -1,24 +1,10 @@
 (() => {
   const SLOT = (window.SLOT = window.SLOT || {});
-  const C = () => SLOT.config;
-
-  function getSoundOn() {
-    try { return localStorage.getItem(C().STORAGE_KEYS.sound) !== "0"; }
-    catch (_) { return true; }
-  }
-
-  function setSoundOn(v) {
-    const on = !!v;
-    try { localStorage.setItem(C().STORAGE_KEYS.sound, on ? "1" : "0"); } catch(_) {}
-    return on;
-  }
-
   SLOT.audio = {
-    getSoundOn,
-    setSoundOn,
-    // 필요하면 여기에 효과음 추가
+    setOn(_) {},
     playClick() {},
     playSpin() {},
     playWin() {},
+    playJackpot() {},
   };
 })();
